18387060b4fbeb727138528ee14b34fbfea9bd81
Ward Alhadid <wf.hadid94@gmail.com>
2021-12-30 12:05:56 -0500

    intial

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+  const colors = [
+    '#16a085',
+    '#27ae60',
+    '#2c3e50',
+    '#f39c12',
+    '#e74c3c',
+    '#9b59b6',
+    '#FB6964',
+    '#342224',
+    '#472E32',
+    '#BDBB99',
+    '#77B1A9',
+    '#73A857'
+  ];
+  const bgColor = colors[Math.floor(Math.random() * colors.length)];
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+	const quotesArray = async () => {
+    await fetch("https://type.fit/api/quotes")
+	 .then( (response) => response.json())
+	 .then((data) => {
+     const random = Math.floor(Math.random() * data.length)
+     setQuote(data[random].text);
+     setAuthor(data[random].author);
+    });
+	};
+	useEffect(() => {	quotesArray(); }, []);
+  const handleOnClick = () => {
+    quotesArray();
+  };
+  return (
+    <div style={{background: bgColor}} className='main'>
+    <div className="container">
+      <div>
+        <p style = {{color: bgColor}}> <i class="fa-duotone fa-quote-left fa"> </i>
+          {` ${quote} `} 
+        <i class="fa-duotone fa-quote-right fa"></i> </p>
+        <p className="author" style={{color: bgColor}}> - {author}  </p>
+			</div>
+      <div className="buttons">
+        <a
+         href= {"https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + quote + "- " + author}
+          target="_noopener"
+          style={{color: bgColor}}
+          >
+         <i className='fab fa-twitter fa-2x'></i> 
+        </a>
+			<button
+      className='btn btn-dark'
+       onClick = {handleOnClick}
+      >New Quote </button>
+			</div>
+		</div>
+    </div>
+  );
+}
+
+export default App;
